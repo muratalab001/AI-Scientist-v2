a85205a16da8ca6b2be17e3a7d40f92818f59e7d
MasakiMurata1982 <m.murata@sit.ac.jp>
2025-05-27 21:24:40 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+python3 launch_scientist_bfts.py \
+ --load_ideas "ai_scientist/ideas/i_cant_believe_its_not_better.json" \
+ --load_code \
+ --add_dataset_ref \
+ --model_writeup o1-preview-2024-09-12 \
+ --model_citation gpt-4o-2024-11-20 \
+ --model_review gpt-4o-2024-11-20 \
+ --model_agg_plots o3-mini-2025-01-31 \
+ --num_cite_rounds 20

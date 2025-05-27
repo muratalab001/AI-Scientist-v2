7ef4bfc86e20bebb67b7edd5b3a01ccaeb7d5a55
muratalab001 <muratalab001@sit.ac.jp>
2025-05-27 15:37:29 +0900

    Update launch_docker.py

--- muratalab/launch_docker.py
+++ muratalab/launch_docker.py
@@ -40,7 +40,8 @@ def main():
         # detach=False にするとコマンド完了まで待機し標準出力を返却
         output = client.containers.run(
             image=image_name,
-            command=["python3", "./muratalab/test.py"],
+            #command=["python3", "./muratalab/test.py"],
+            command = ["python3", "--version"],
             remove=True,               # --rm
             ports=ports,
             volumes=volumes,

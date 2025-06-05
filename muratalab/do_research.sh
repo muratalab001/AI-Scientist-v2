53211526cea780e0020d3f4596d381db82cf7d5f
muratalab001 <muratalab001@sit.ac.jp>
2025-06-05 19:52:46 +0900

    chage json file path

--- muratalab/do_research.sh
+++ muratalab/do_research.sh
@@ -1,5 +1,5 @@
 python3 launch_scientist_bfts.py \
- --load_ideas "ai_scientist/ideas/PINN_TopologicalSoliton.json" \
+ --load_ideas "ai_scientist/ideas/PINN_TopologicalSoliton2.json" \
  --add_dataset_ref \
  --model_writeup o1-preview-2024-09-12 \
  --model_citation gpt-4o-2024-11-20 \
--- muratalab/loop.py
+++ muratalab/loop.py
@@ -1,5 +1,5 @@
 import launch_docker
 
-for i in range(1):
+for i in range(5):
     print(str(i+1)+"-th trial")
     launch_docker.main()
